d0159976587d28da8f7edfb59dcd9a7a4767da8e
julia Wager <julia@p-wagner.at>
2023-11-26 12:57:54 +0100

    add allauth

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['django-blog-julia-wagner-53a1c3c5074b.herokuapp.com',
                  'django-blog-julia-wagner.herokuapp.com/',
-                 'localhost']
+                 'localhost',
+                 '127.0.0.1:8000/']
 
 
 # Application definition
@@ -43,12 +44,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'codestar.urls'
